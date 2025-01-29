93cf65e1f5d4a9b1f9bb9d29b02fd819c6f3cad0
karlhelen <karlhelen488@gmail.com>
2025-01-29 09:39:25 +0000

    update structure

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.page}>
+      <div className={styles.banner}>
+        <h1>Level up your crypto portfolio</h1>
+      </div>
+    </main>
+  );
+}
